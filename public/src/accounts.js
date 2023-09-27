1cc12e718fbffac50da6ccaa24caea4237793b0e
Ryan McGeeney <ryanmcgeeney@yahoo.com>
2023-09-27 12:58:13 -0500

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,60 @@
+function findAccountById(accounts, id) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  const foundItem = accounts.find((element) => element.id == id);
+  return foundItem
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  accounts.sort( function (first,second) { 
+  if (first.name.last < second.name.last) {
+    return -1;
+  }
+  if (first.name.last > second.name.last) {
+    return 1;
+  }
+  return 0;
+})
+return accounts  
+
+}
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+names = accounts.map(author => `${author.name.first} ${author.name.last}`);
+return names  
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
